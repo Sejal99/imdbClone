39f2fc479fb3ba04ffd329f256de38b595bd236a
sejal srivastava <sejal8974@gmail.com>
2023-04-22 12:35:31 +0530

    first commit

---
+++ src/components/movieList/movieList.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react"
+import "./movieList.css"
+import { useParams } from "react-router-dom"
+import Cards from "../card/card"
+
+const MovieList = () => {
+    
+    const [movieList, setMovieList] = useState([])
+    const {type} = useParams()
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    useEffect(() => {
+        getData()
+    }, [type])
+// when we are switching pages so if any type is specifies we go to that page else we go to default page that is popular
+    const getData = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieList(data.results))
+    }
+            //we map the movies and render them one by one in the card
+    return (
+        <div className="movie__list">
+            <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+            <div className="list__cards">
+                {
+                    movieList.map(movie => (
+                        <Cards movie={movie} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MovieList
